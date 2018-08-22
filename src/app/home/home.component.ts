81ded89fe4042428b858b49d80ff73c2d6d65bd2
Uriel <ur13l.infante@gmail.com>
2018-08-22 13:26:27 -0500

    [UPDATE] TEST

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { trigger, transition, useAnimation } from '@angular/animations';
 import { fadeIn } from 'ng-animate';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +14,13 @@ import { fadeIn } from 'ng-animate';
   ],
 })
 export class HomeComponent implements OnInit {
-
+  public user : User;
   constructor(
-    private title : Title
-  ) { }
+    private title : Title,
+    private userService : UserService
+  ) { 
+    this.user = this.userService.getUser();
+  }
 
   ngOnInit() {
     this.title.setTitle("R O C K T E C H")
